8db8bc8ffa3a9d1a9fa5e2ac61d4962b769686b0
HjalmarSnoep <github@snoep.at>
2017-10-03 15:32:08 +0200

    commentaar gegeven.

--- sanitising/sanitising.php
+++ sanitising/sanitising.php
@@ -2,10 +2,12 @@
 
 echo "<h1>Sanitised variables: </h1>";
 /* Start code by Hjalmar Snoep */
+// references:
+// https://stackoverflow.com/questions/386294/what-is-the-maximum-length-of-a-valid-email-address
 if(isset($_REQUEST["email"]))
 {
 	$email = filter_var($_REQUEST["email"], FILTER_SANITIZE_EMAIL);
-	$email=substr($email,0,50);// no longer than 50.
+	$email=substr($email,0,254);// no longer than 254 characters.
 	echo "EMAIL: '".$email."'<br>"; 
 }else
 {
@@ -30,6 +32,7 @@ $gender = filter_var ( $_REQUEST["gender"], FILTER_SANITIZE_STRING);;
 echo "Gender: " . $gender;
 //Did you assume my gender?
 // LEUKE grap, nu serieus: er zijn drie mogelijkheden, dus FILTER_SANITIZE_STRING is een beetje ruim.
+// ook heb je niet beperkt in ruimte..
 /* End of code by Stef van Egmond */
 
 
@@ -40,6 +43,9 @@ if (isset($_POST['submit'])) {
   $range = filter_var($_POST["range"], FILTER_SANITIZE_NUMBER_INT);
   echo $range;
 }
+// LETOP: ik heb step toegevoegd, deze code was op zich correct, wie durft range met step=0.1 WEL aan?
+// MISSING: casting /  type juggling?
+// MISSING: beperking qua ruimte?
 /* End Code by Mark van Dooremaal */
 
 
@@ -59,6 +65,8 @@ if(isset($_REQUEST["date"]))
 } else {
 	echo "Date: '' (not set)<br>";
 }
+// MISSING: beperking qua aantal characters, toevoegen VOOR date_parse.
+// verder: nice!
 /* End of code by Simon Boerrigter */
 
 
